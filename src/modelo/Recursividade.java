5fa487aa944adcae4cad63407c5fa733aab0ceb6
Carlos Massavanhane <cmassavanhanejr@gmail.com>
2018-08-31 23:06:01 +0200

    Adicionados tres metodos
    Metodo calcularFactorial;
    Metodo somaElementos;
    Metodo multiplicar;

--- src/modelo/Recursividade.java
+++ src/modelo/Recursividade.java
@@ -3,6 +3,8 @@
  */
 package modelo;
 
+import java.util.Vector;
+
 /**
  * @author Carlos Massavanhane
  *
@@ -10,7 +12,7 @@ package modelo;
 public class Recursividade {
 	
 	/**
-	 * Metodo para calcular potencia de um numero
+	 * Metodo para calcular potencia de um numero dado
 	 * @param b - base 
 	 * @param e - expoente
 	 * @return valor da potencia
@@ -22,4 +24,35 @@ public class Recursividade {
 		else 
 			return b* calcularPotencia(b,e-1);
 	}
+	
+	/**
+	 * Metodo para calcular factorial de um numero dado
+	 * @param n
+	 * @return
+	 */
+	public static int calcularFactorial(int n) {
+		if(n==0)
+			return 1;
+		else
+			return n * calcularFactorial(n-1);
+	}
+	
+	/**
+	 * Metodo para somar elementos de um vector de inteiros
+	 * @param vecto - Vector
+	 * @param nElem - numero de elementos
+	 */
+	public static int somaElementos(Vector<Integer> vector, int nElem ) {
+		if(nElem==0)
+			return 0;
+		else
+			return vector.get(--nElem).intValue()+somaElementos(vector, nElem--);
+	}
+	
+	public static int multiplicar(int a, int b) {
+		if(b==0)
+			return 0;
+		else
+			return a+Recursividade.multiplicar(a, --b);	}
+	
 }
--- src/view/TestaRecursividade.java
+++ src/view/TestaRecursividade.java
@@ -3,6 +3,8 @@
  */
 package view;
 
+import java.util.Vector;
+
 import modelo.Recursividade;
 
 /**
@@ -18,7 +20,20 @@ public class TestaRecursividade {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//testando metodo potecnia recursivo
 		System.out.println(Recursividade.calcularPotencia(10, 3));
+		//testando metodo factorial recursivo
+		System.out.println(Recursividade.calcularFactorial(8));
+		//Testando metodo somar recursivo
+		Vector<Integer> vector=new Vector<>();
+		vector.add(new Integer(1));
+		vector.add(new Integer(2));
+		vector.add(new Integer(3));
+		vector.add(new Integer(4));
+		System.out.println(Recursividade.somaElementos(vector, 4));
+		
+		/*Testando metodo multipolicar recursivo*/
+		System.out.println(Recursividade.multiplicar(4, 3));
 	}
 
 }
